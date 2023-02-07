f6f0a388c252f0d7b203c1c9df04a8334af7d61c
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2023-02-07 16:02:57 +0100

    Add MVVM architecture

---
+++ O2O/O2O/Details/DetailsViewModel.swift
@@ -0,0 +1,20 @@
+//  DetailsViewModel.swift
+//  O2O
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import Foundation
+
+class DetailsViewModel {
+    
+    // MARK: - Properties
+    
+    weak var view: DetailsViewControllerProtocol?
+    var router: DetailsRouter?
+    
+    // MARK: - Helpers
+    
+}
