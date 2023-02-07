bcfc015cd0a870f9cd6ca05ba03bfe199edf1aa3
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2023-02-07 16:00:16 +0100

    Initial commit Swift

---
+++ O2O/O2O/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  O2O
+//
+//  Created by Oscar Rodriguez Garrucho on 7/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
